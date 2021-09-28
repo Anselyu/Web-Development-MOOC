4ef4688660a040592fccf9285d1d9b0011ba52c2
Carl Ansel Yu <carlayu10@gmail.com>
2021-09-28 15:57:17 -0400

    Added audio classes for every audio file

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,6 +1,18 @@
+var mainButtons =  document.querySelectorAll(".set button");
+
+var crash = new Audio('sounds/crash.mp3');
+var kick_bass = new Audio("sounds/kick-bass.mp3");
+var snare = new Audio("sounds/snare.mp3");
+var tom_1 = new Audio("sounds/tom-1.mp3");
+var tom_2 = new Audio("sounds/tom-2.mp3");
+var tom_3 = new Audio("sounds/tom-3.mp3");
+var tom_4 = new Audio("sounds/tom-4.mp3");
+
+
+
 for (var i = 0; i < 7; i++){
-    document.querySelectorAll(".set button")[i].addEventListener("click", function (){
-        alert("I got clicked!");
+    mainButtons[i].addEventListener("click", function (){
+    
     });
 }
 
