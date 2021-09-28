6a42f3d721975b0dc9f6c83fcacf8deed0309c3f
Carl Ansel Yu <carlayu10@gmail.com>
2021-09-28 17:07:37 -0400

    Added keydown listeners

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -12,29 +12,39 @@ var tom_4 = new Audio("sounds/tom-4.mp3");
 
 for (var i = 0; i < 7; i++){
     mainButtons[i].addEventListener("click", function (){
-        switch(this.innerHTML){
-            case 'w':
-                tom_1.play();
-                break;
-            case 'a':
-                tom_2.play();
-                  break;
-            case 's':
-                tom_3.play();
-                break;
-            case 'd':
-                tom_4.play();
-                break;
-            case 'j':
-                snare.play();
-                break;
-            case 'k':
-                crash.play();
-                break;
-            default:
-                kick_bass.play();
-        }
+      playSound(this.innerHTML);
     });
 }
 
+document.addEventListener("keydown", function(event){
+    playSound(event.key);
+})
+
+function playSound(key) {
+    switch(key){
+        case 'w':
+            tom_1.play();
+            break;
+        case 'a':
+            tom_2.play();
+            break;
+        case 's':
+            tom_3.play();
+            break;
+        case 'd':
+            tom_4.play();
+            break;
+        case 'j':
+            snare.play();
+            break;
+        case 'k':
+            crash.play();
+            break;
+        case 'l':
+            kick_bass.play();
+            break;
+        default:
+            console.log(this.innerHTML);       
+    }
+}
 
