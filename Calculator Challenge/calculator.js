48444a2d0cb6df97fedcf0c440614391063709cf
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-06 14:59:27 -0400

    Used express body parser to create a web application that adds two numbers

--- Calculator Challenge/calculator.js
+++ Calculator Challenge/calculator.js
@@ -1,10 +1,20 @@
 const express = require("express");
+
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
 app.get("/",function(req, res){
-    res.send("Hello World");
+    res.sendFile(__dirname + "/index.html");
 })
 
+app.post("/", function(req, res){
+
+    num1 = Number(req.body.num1);
+    num2 = Number(req.body.num2);
+
+
+    res.send("Your number is: " + (num1 + num2));
+})
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 })
