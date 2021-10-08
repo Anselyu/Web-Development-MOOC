80deb6082d96c14deefb7b7baa8aa6ea5edc1440
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-08 17:20:18 -0400

    Utilized the data to display text and image tags on website

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const https = require("https");
-
+app.use(express.urlencoded({extended: true}));
 
 const app = express();
 
 app.get("/", function(req, res){
 
-    url = "https://api.openweathermap.org/data/2.5/weather?q=Markham&id=524901&appid=dd1c9c5f8d67c7fc5dd9ab250b2b6e6b";
+    url = "https://api.openweathermap.org/data/2.5/weather?q=Markham&id=524901&appid=dd1c9c5f8d67c7fc5dd9ab250b2b6e6b&units=metric";
 
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -15,11 +15,20 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
+            const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"; 
+            
+            
+            
+          
+            res.write("<h1>The temperature in Markham is " + temp + " degrees.</h1>");
+            res.write("<p>The weather is currently: " + description + ".</p>");
+            res.write("<img src=" + icon +">");
+            res.send();
         })
     });
 
 
-    res.sendFile(__dirname + "/index.html");
+    // res.sendFile(__dirname + "/index.html");
 })
 
 
