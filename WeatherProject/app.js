8a79135c0561367f985be25c474f2d58c98f6c88
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-09 07:56:40 -0400

    added buttons and dropdown menu for user to select city name and unit type

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const https = require("https");
-app.use(express.urlencoded({extended: true}));
+
 
 const app = express();
 
+
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
 
-    url = "https://api.openweathermap.org/data/2.5/weather?q=Markham&id=524901&appid=dd1c9c5f8d67c7fc5dd9ab250b2b6e6b&units=metric";
+   
+
+
+res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res){
+    const location = req.body.city;
+    const unitType = req.body.unitType;
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&id=524901&appid=dd1c9c5f8d67c7fc5dd9ab250b2b6e6b&units=" + unitType;
 
     https.get(url, function(response){
         console.log(response.statusCode);
 
         response.on("data" , function(data){
+        
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
@@ -20,15 +33,12 @@ app.get("/", function(req, res){
             
             
           
-            res.write("<h1>The temperature in Markham is " + temp + " degrees.</h1>");
+            res.write("<h1>The temperature in " + location + " is " + temp + " degrees.</h1>");
             res.write("<p>The weather is currently: " + description + ".</p>");
             res.write("<img src=" + icon +">");
             res.send();
         })
     });
-
-
-    // res.sendFile(__dirname + "/index.html");
 })
 
 
