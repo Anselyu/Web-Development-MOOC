dbd4f651c9e1f0882e6fbafb8a7a694832ee50be
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-09 09:06:26 -0400

    Made web application runnable via server and used body parser to obtain information from form

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const https = require("https");
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/" , function(req , res){
+    firstName = req.body.firstName;
+    lastName = req.body.lastName;
+    email = req.body.email;
+
+    console.log("First Name: " + firstName + " Last Name: " + lastName + " Email: " + email);
+});
 
 
 app.listen(3000, function(){
