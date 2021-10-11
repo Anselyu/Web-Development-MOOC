622b52ef79db3581f1fe81af5452554738ef09f5
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-11 13:39:21 -0400

    Added functionality with mailchimp API to allow automatic newsletters

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,19 +4,40 @@ const https = require("https");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+mailchimp.setConfig({
+  apiKey: "63e75f0921462737554f3afa423e5d72",
+  server: "us5"
+});
+
+
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/" , function(req , res){
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
+app.post('/', (req, res) => {
 
-    console.log("First Name: " + firstName + " Last Name: " + lastName + " Email: " + email);
-});
-
+    var userData = {
+    members: [{
+      email_address: req.body.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: req.body.firstName,
+        LNAME: req.body.lastName
+      }
+    }],};
+   
+   const run = async () => {
+      const response = await mailchimp.lists.batchListMembers("138ef0e7c4", userData );
+      console.log(response);
+    };
+    run();
+   
+  })
 
 app.listen(3000, function(){
     console.log("Listening on port 3000");
 })
+
