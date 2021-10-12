8b31e30bc5d03ce8d9ce94fd83350008761c8dde
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-12 15:33:50 -0400

    Move date functionality to external module

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const date = require(__dirname + "/date.js")
 const app = express();
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -10,18 +12,7 @@ var workItems = [];
 var newItem = "";
 
 app.get("/", function(req, res){
-
-    let today = new Date();
-
-    let options = {
-        day: "numeric",
-        weekday: "long",
-        month: "long"
-    }
-
-    let currentDay = today.toLocaleDateString("en-US", options);
-
-    res.render('list', {listTitle: currentDay, newItems: items});
+    res.render('list', {listTitle: date.getDate(), newItems: items});
 });
 
 app.post("/",function(req,res){
