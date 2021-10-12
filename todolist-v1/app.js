7e0f77afe602e2c1f51f3e38a5f83ee0a6e7e3b0
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-12 13:59:53 -0400

    Add list and option to append to to-do list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,15 +4,30 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+//const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var items = [];
+var newItem = "";
+
 app.get("/", function(req, res){
 
     var today = new Date();
-    var currentDay = daysOfWeek[today.getDay()];
 
-    res.render('list', {currentDay: currentDay});
+    var options = {
+        day: "numeric",
+        weekday: "long",
+        month: "long"
+    }
+
+    var currentDay = today.toLocaleDateString("en-US", options);
+
+    res.render('list', {currentDay: currentDay, newItems: items});
 });
 
+app.post("/",function(req,res){
+    newItem = req.body.newItem;
+    items.push(newItem);
+    res.redirect("/");
+})
 
 
 
