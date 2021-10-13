dc97bae75e1a0e88ed95f844841a1cb29a2f9d57
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-13 12:30:28 -0400

    Add about and contact pages

--- Blogsite Challenge/app.js
+++ Blogsite Challenge/app.js
@@ -14,6 +14,13 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent});
 });
+app.get("/about", function(req, res){
+  res.render("about", {startingContent: aboutContent});
+});
+app.get("/contact", function(req, res){
+  res.render("contact", {startingContent: contactContent});
+});
+
 
 
 
