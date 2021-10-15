3125ba2c4b9293d1a90be1cd26173d23851008b5
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-14 21:43:40 -0400

    Use MongoDB native driver to create a database with three fruit objects

---
+++ FruitsProject/app.js
@@ -0,0 +1,62 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require("assert");
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'fruitsDB';
+
+const client = new MongoClient(url);
+
+client.connect(function(err){
+    assert.equal(null,err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    insertDocuments(db, function() {
+      client.close();
+    });
+});
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([
+        {
+            name : "Apple",
+            score: 5,
+            review: "Kinda bad fruit"
+        }, 
+        {
+            name : "Orange",
+            score: 8,
+            review: "Much better fruit"
+        },    
+        {
+            name : "Banana",
+            score: 9,
+            review: "Great stuff!"
+        }
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3,result.insertedCount);
+        assert.equal(3,Object.keys(result.insertedIds).length);
+
+
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+  }
+
+  const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function(err, fruits) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(fruits)
+      callback(fruits);
+    });
+  }
