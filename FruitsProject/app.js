fb38620ed48ce4424cdab0c71485b922e55e4e92
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-18 02:27:07 -0400

    add validation to data

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,11 +1,21 @@
 
+
 const mongoose = require('mongoose');
+const { MongoServerClosedError } = require('mongoose/node_modules/mongodb');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
 
+
 const fruitsSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Why no name?"]
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10
+  },
   review: String
 })
 
@@ -17,32 +27,7 @@ const apple = new Fruit({
   review: "Not very good"
 });
 
-const banana = new Fruit({
-  name: "Banana",
-  rating: 10,
-  review: "Greatest fruit ever!"
-});
-
-const grape = new Fruit({
-  name: "Grapes",
-  rating: 8,
-  review: "Pretty decent"
-});
-
-const grapefruit = new Fruit({
-  name: "Grapefruit",
-  rating: 1,
-  review: "Terrible"
-});
-
-// Fruit.insertMany([banana, grape, grapefruit], function(err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("Successfully saved fruits to database");
-//   }
-// })
-//apple.save();
+// apple.save();
 
 const personsSchema = new mongoose.Schema({
   name: String,
@@ -63,6 +48,39 @@ Fruit.find(function(err, fruits){
   if (err){
     console.log(err);
   } else {
-    console.log(fruits);
+    mongoose.connection.close();
+    fruits.forEach(function(f){
+      console.log(f.name);
+      
+    })
+    
   }
 });
+
+
+
+// const banana = new Fruit({
+//   name: "Banana",
+//   rating: 10,
+//   review: "Greatest fruit ever!"
+// });
+
+// const grape = new Fruit({
+//   name: "Grapes",
+//   rating: 8,
+//   review: "Pretty decent"
+// });
+
+// const grapefruit = new Fruit({
+//   name: "Grapefruit",
+//   rating: 1,
+//   review: "Terrible"
+// });
+
+// Fruit.insertMany([banana, grape, grapefruit], function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved fruits to database");
+//   }
+// })
