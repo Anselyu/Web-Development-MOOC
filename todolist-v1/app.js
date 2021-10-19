35ad6428762380290a0a7b26dc6d15d1f06597d2
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-18 22:39:01 -0400

    use mongoose to retrieve list items from database

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,11 +6,44 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const mongoose = require('mongoose');
+const { MongoServerClosedError } = require('mongoose/node_modules/mongodb');
+mongoose.connect("mongodb://localhost:27017/listDB");
+
+const itemsSchema = new mongoose.Schema({
+    content: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const workItem = mongoose.model("WorkItem", itemsSchema);
+
 const items = ["Eat", "Sleep"];
 const workItems = [];
 
+//function updateDatabase()
+
+// const c = new Item({
+//     content: "Eat"
+// })
+// const a = new Item({
+//     content: "Eat"
+// })
+// const b = new Item({
+//     content: "Eat"
+// })
+
+// Item.insertMany([a, b, c], function(err){
+    
+// })
+
+
+
+
 app.get("/", function(req, res){
-    res.render('list', {listTitle: date.getDate(), newItems: items});
+    Item.find({}, function(err, foundItems){
+        res.render('list', {listTitle: date.getDate(), newItems: foundItems});
+    });
 });
 
 app.post("/",function(req,res){
