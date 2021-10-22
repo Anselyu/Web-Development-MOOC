43bf7ffe19200c9a57263abc02d9642beea54512
Carl Ansel Yu <carlayu10@gmail.com>
2021-10-21 22:05:08 -0400

    Initial setup commit

---
+++ Wiki-API/app.js
@@ -0,0 +1,27 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+// const _ = require("lodash");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const mongoose = require('mongoose');
+const { MongoServerClosedError } = require('mongoose/node_modules/mongodb');
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/", function(req, res){
+    res.send("hello world");
+})
+
+app.listen(3000, function(){
+    console.log("App listening on port 3000");
+})
