1c2bbdbb9505f57843a068095e1ad3f6b92c9565
Carl Ansel Yu <carlayu10@gmail.com>
2021-11-06 15:29:34 -0400

    Reformat code using arrow function

--- Emojipedia React Challenge/emojipedia-react-challenge/src/components/App.jsx
+++ Emojipedia React Challenge/emojipedia-react-challenge/src/components/App.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import Term from "./Term";
 import emojipedia from "../emojipedia";
 
-function createTerm(term){
-    return (
-        <Term key={term.id} emoji={term.emoji} name={term.name} meaning={term.meaning} />
-    )
-}
 function App() {
   return (
     <div>
@@ -14,7 +9,14 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <dl className="dictionary">
-        {emojipedia.map(createTerm)}
+        {emojipedia.map((term) => (
+            <Term 
+                key={term.id} 
+                emoji={term.emoji} 
+                name={term.name} 
+                meaning={term.meaning} 
+            />
+        ))}
       </dl>
     </div>
   );
