0f353977cff3df7783f057b863496649b71c22c2
Carl Ansel Yu <carlayu10@gmail.com>
2021-11-10 16:10:35 -0500

    Conditionally render create area component based on whether user clicks on textarea or not

--- my-app/src/components/CreateArea.jsx
+++ my-app/src/components/CreateArea.jsx
@@ -6,6 +6,7 @@ import { Zoom } from '@material-ui/core'
 function CreateArea(props) {
   const [noteTitle, setNoteTitle] = useState();
   const [noteContent, setNoteContent] = useState();
+  const [isClicked, setClicked] = useState(false);
 
   function onChange(event) {
     const {name, value} = event.target;
@@ -19,20 +20,25 @@ function CreateArea(props) {
   return (
     <div>
       <form className="create-note">
-        <input
-          onChange={onChange}
-          name="title"
-          placeholder="Title"
-          value={noteTitle}
-        />
+        {isClicked && 
+          <input
+            onChange={onChange}
+            name="title"
+            placeholder="Title"
+            value={noteTitle}
+          />
+        }
         <textarea
+          onClick={() => {
+            setClicked(!isClicked);
+          }}
           onChange={onChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isClicked ? "3" : "1"}
           value={noteContent}
         />
-        <Zoom in={true}>
+        <Zoom in={isClicked}>
           <Fab onClick={(event) => {
             props.onAdd({
             title: noteTitle,
