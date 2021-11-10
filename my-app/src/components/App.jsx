f8153c0d9d1cebaac820db608a13f59f00ad9a6d
Carl Ansel Yu <carlayu10@gmail.com>
2021-11-10 14:29:47 -0500

    Allow user to add new note

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
-function App(){
-    return (
-        <div>
-            <Header />
-            <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
-            <Footer />
-        </div>
-    )
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    //console.log(newNote);
+    setNotes((prevNotes) => {
+      //.log([...prevNotes]);
+      return [...prevNotes, newNote];
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note) => {
+        //console.log(note.title);
+        return <Note title={note.title} content={note.content} />;
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- my-app/src/components/CreateArea.jsx
+++ my-app/src/components/CreateArea.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const test = {
+    title: "Note Title",
+    content: "Note Content"
+  };
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(event) => {
+          props.onAdd(test);
+          event.preventDefault();
+        }}
+      >
         <input name="title" placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
