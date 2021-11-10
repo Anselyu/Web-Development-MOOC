d8cb6344e0f9b044d042adfc78fff9a0670634cb
Carl Ansel Yu <carlayu10@gmail.com>
2021-11-10 16:15:30 -0500

    Finalize project by adding background image and adding logo

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import HighlightIcon from '@material-ui/icons/Highlight'
 
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
@@ -7,7 +7,7 @@ import React from "react";
 function Header(){
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1><HighlightIcon />Keeper</h1>
         </header>);
 }
 
